c3396f65035a9f80c855891108869a58286c68be
ElbertF <info@elbertf.com>
2011-01-26 19:33:20 +1100

    -

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -21,9 +21,17 @@ transmission &
 
 nicotine &
 
+thunar ~/music &
+
 terminator -e alsamixer -T "Alsa Mixer" &
 
 (sleep 1s && vlc) &
+
+## VNC server
+(sleep 30s && /usr/lib/vino/vino-server) &
+
+# Dropbox deamon
+(sleep 60s && ~/.dropbox-dist/dropboxd) &
 
 ## cb-fortune - have Statler say a little adage
 #(sleep 120s && cb-fortune) &
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,34 +7,13 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Layout.NoBorders
 import System.IO
 import qualified XMonad.StackSet as W -- to shift and float windows
- 
--- manageHook
-myManageHook :: ManageHook
 
-myManageHook = composeAll . concat $
-    [ [className =? c --> doFloat | c <- myFloats]
-    , [title     =? t --> doFloat | t <- myOtherFloats]
-    , [resource  =? r --> doFloat | r <- myIgnores]
-    , [className =? "Firefox"      --> doF (W.shift "2-WEB")]
-    , [className =? "Minefield"    --> doF (W.shift "2-WEB")]
-    , [className =? "Iceweasel"    --> doF (W.shift "2-WEB")]
-    , [className =? "Gvim"         --> doF (W.shift "3-EDIT")]
-    , [className =? "Vlc"          --> doF (W.shift "4-MEDIA")]
-    , [title     =? "Alsa Mixer"   --> doF (W.shift "4-MEDIA")]
-    , [className =? "Transmission" --> doF (W.shift "6-PIRATE")]
-    , [className =? "Nicotine"     --> doF (W.shift "6-PIRATE")]
-    ]
-    where
-    myFloats      = ["Gimp", "gimp"]
-    myOtherFloats = ["Downloads", "Firefox Preferences", "Save As..."]
-    myIgnores     = []
-
--- The main function.
+-- The main function
 main = do
 	spawn "sh ~/.xmonad/autostart.sh"
 	xmproc <- spawnPipe "xmobar ~/.xmobarrc"
 	xmonad $ defaultConfig
-		{ workspaces = ["1-CLI","2-WEB","3-EDIT","4-MEDIA","5","6-PIRATE","7","8","9","0","-","="]
+		{ workspaces = myWorkspaces
 		, manageHook = myManageHook <+> manageHook defaultConfig -- uses default too
 		, layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
 		, logHook    = dynamicLogWithPP $ xmobarPP
@@ -44,13 +23,53 @@ main = do
 			, ppTitle   = xmobarColor "#9F0" "" . shorten 50
 			}
 		, modMask    = mod4Mask
-        } `additionalKeys`
-		[ ((mod4Mask, xK_w),     spawn "~/apps/firefox/firefox")
-		, ((mod4Mask, xK_g),     spawn "gimp")
-		, ((mod4Mask, xK_m),     spawn "vlc")
-		, ((mod4Mask, xK_t),     spawn "terminator")
-		, ((0,        xK_Print), spawn "scrot")
-		, ((0,        xK_F11),   spawn "amixer --quiet set Master 3-")
-		, ((0,        xK_F12),   spawn "amixer --quiet set Master 3+")
-		, ((mod4Mask, xK_s),     spawn "amixer --quiet set Master toggle")
-		]
+        } `additionalKeys` myKeys
+
+myWorkspaces = ["1-CLI","2-WEB","3-EDIT","4-MEDIA","5","6-PIRATE","7","8","9","0","-","="]
+
+-- Organize windows
+myManageHook :: ManageHook
+
+myManageHook = composeAll . concat $
+    [ [className =? c --> doFloat | c <- myFloats]
+    , [title     =? t --> doFloat | t <- myOtherFloats]
+    , [resource  =? r --> doFloat | r <- myIgnores]
+    , [className =? "Firefox"              --> doF (W.shift "2-WEB")]
+    , [className =? "Minefield"            --> doF (W.shift "2-WEB")]
+    , [className =? "Iceweasel"            --> doF (W.shift "2-WEB")]
+    , [className =? "Gvim"                 --> doF (W.shift "3-EDIT")]
+    , [className =? "Vlc"                  --> doF (W.shift "4-MEDIA")]
+    , [title     =? "Alsa Mixer"           --> doF (W.shift "4-MEDIA")]
+    , [title     =? "music - File Manager" --> doF (W.shift "4-MEDIA")]
+    , [className =? "Transmission"         --> doF (W.shift "6-PIRATE")]
+    , [className =? "Nicotine"             --> doF (W.shift "6-PIRATE")]
+    ]
+    where
+    myFloats      = ["Gimp", "gimp"]
+    myOtherFloats = ["Downloads", "Firefox Preferences", "Save As..."]
+    myIgnores     = []
+
+-- Key bindings
+myKeys =
+	[ ((mod4Mask, xK_w),     spawn "~/apps/firefox/firefox")
+	, ((mod4Mask, xK_f),     spawn "thunar")
+	, ((mod4Mask, xK_g),     spawn "gimp")
+	, ((mod4Mask, xK_m),     spawn "vlc")
+	, ((mod4Mask, xK_t),     spawn "terminator")
+	, ((0,        xK_Print), spawn "scrot")
+	, ((0,        xK_F11),   spawn "amixer --quiet set Master 3-")
+	, ((0,        xK_F12),   spawn "amixer --quiet set Master 3+")
+	, ((mod4Mask, xK_s),     spawn "amixer --quiet set Master toggle")
+	, ((mod4Mask, xK_b),     sendMessage ToggleStruts)
+	]
+    ++
+    [ ((m .|. mod4Mask, k), windows $ f i) | (i, k) <- zip myWorkspaces numPadKeys
+    , (f, m)                                        <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    ]
+
+-- Non-numeric num pad keys, sorted by number
+numPadKeys = [ xK_KP_End,  xK_KP_Down,  xK_KP_Page_Down -- 1, 2, 3
+             , xK_KP_Left, xK_KP_Begin, xK_KP_Right     -- 4, 5, 6
+             , xK_KP_Home, xK_KP_Up,    xK_KP_Page_Up   -- 7, 8, 9
+             , xK_KP_Insert                             -- 0 
+			 ]
