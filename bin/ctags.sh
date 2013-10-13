3313e39410d24b6444ae111399c8847f4ad559ce
Elbert Alias <elbert@alias.io>
2013-10-13 17:21:45 +1100

    -

--- bin/ctags.sh
+++ bin/ctags.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-dir="/home/elbert/www/"
+dir='/home/elbert/www/'
 
 for file in `find $dir -mindepth 1 -maxdepth 2 -name tags -type f`
 do
-	/usr/bin/ctags -R $(dirname $file)
+	/usr/bin/ctags -f tags \
+    -h '.php' -R \
+    --PHP-kinds=cfiv \
+    --totals=yes \
+    --tag-relative=yes \
+    --PHP-kinds=cfiv \
+    --regex-PHP='/(abstract)?\s+class\s+([^ ]+)/\2/c/' \
+    --regex-PHP='/interface\s+([^ ]+)/\1/i/' \
+    --exclude='*.js' \
+    --exclude='.git'
 done
